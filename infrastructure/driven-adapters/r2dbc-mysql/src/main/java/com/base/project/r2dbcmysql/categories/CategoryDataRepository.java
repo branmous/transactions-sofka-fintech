191f42b18c90c8f90c93bb5cf58c92b8377f9b73
branmous <brandon-377@hotmail.com>
2026-01-09 00:04:40 -0500

    Remove unused categories and photos components, including models, repositories, and services; update build configurations.

--- infrastructure/driven-adapters/r2dbc-mysql/src/main/java/com/base/project/r2dbcmysql/categories/CategoryDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.base.project.r2dbcmysql.categories;
-
-import org.springframework.data.repository.query.ReactiveQueryByExampleExecutor;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface CategoryDataRepository extends
-        ReactiveCrudRepository<CategoryData, Long>,
-        ReactiveQueryByExampleExecutor<CategoryData> {
-}
--- infrastructure/driven-adapters/r2dbc-mysql/src/main/java/com/base/project/r2dbcmysql/transactions/TransactionService.java
+++ infrastructure/driven-adapters/r2dbc-mysql/src/main/java/com/base/project/r2dbcmysql/transactions/TransactionService.java
@@ -1,16 +1,10 @@
 package com.base.project.r2dbcmysql.transactions;
 
-import com.base.project.model.photo.Photo;
 import com.base.project.model.transaction.Transaction;
 import com.base.project.model.transaction.gateways.TransactionRepository;
 import com.base.project.r2dbcmysql.helper.ReactiveAdapterOperations;
-import com.base.project.r2dbcmysql.photos.PhotoData;
-import com.base.project.r2dbcmysql.photos.PhotoDataRepository;
-import lombok.RequiredArgsConstructor;
 import org.reactivecommons.utils.ObjectMapper;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Repository
